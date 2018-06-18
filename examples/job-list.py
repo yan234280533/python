5f56a36f084a4652855731b91756c40039ad5b97
devinyan <devinyan@tencent.com>
2018-06-19 00:50:14 +0800

    将日志方式修改为logging输出

--- examples/job-list.py
+++ examples/job-list.py
@@ -1,3 +1,6 @@
+#!/usr/bin/python
+#-*- coding:utf-8 -*-
+
 # Copyright 2016 The Kubernetes Authors.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -15,37 +18,46 @@
 from kubernetes import client, config, watch
 import logging
 import time
+import sys
 
 def delete_job_namespace(client,name,namespace):
     try:
        return client.delete_namespaced_job( name=name, body={}, namespace=namespace)
     except Exception as err:
-        print("delete_job_namespace failed,%s"%(err))
+        logger.info("delete_job_namespace failed,%s"%(err))
         return False
 
 def delete_pod_namespace(client,name,namespace):
     try:
         return client.delete_namespaced_pod(name=name, body={}, namespace=namespace)
     except Exception as err:
-        print("delete_pod_namespace failed,%s" % (err))
+        logger.info("delete_pod_namespace failed,%s" % (err))
         return False
 
 def get_job_namespace(client,name,namespace):
     try:
        return client.read_namespaced_job( name=name, namespace=namespace)
     except Exception as err:
-        print("delete_job_namespace failed,%s"%(err))
+        logger.info("delete_job_namespace failed,%s"%(err))
         return None
 
 def list_pod_namespace(client,namespace,label_selector):
     try:
        return client.list_namespaced_pod(namespace=namespace,label_selector=label_selector)
     except Exception as err:
-        print("list_pod_namespace failed,%s"%(err))
+        logger.info("list_pod_namespace failed,%s"%(err))
         return None
 
 
 def main():
+    # 创建一个logger
+    logger = logging.getLogger('mytest111')
+    logger.setLevel(logging.DEBUG)
+    format = logging.Formatter("%(asctime)s - %(message)s")    # output format
+    sh = logging.StreamHandler(stream=sys.stdout)    # output to standard output
+    sh.setFormatter(format)
+    logger.addHandler(sh)
+
     # Configs can be set in Configuration class directly or using helper
     # utility. If no argument provided, the config will be loaded from
     # default location.
@@ -53,6 +65,11 @@ def main():
 
     v1Batch = client.BatchV1Api()
     v1Core = client.CoreV1Api()
+
+    print("begin print")
+    logger.setLevel(logging.DEBUG)
+    logger.info("begin info")
+    logger.error("begin error")
 
     while 1:
        time.sleep(1)  # 休眠1秒
@@ -63,10 +80,10 @@ def main():
                    if object.spec.completions != None and object.spec.completions == object.status.succeeded:
                        ret = delete_job_namespace(v1Batch, object.metadata.name, object.metadata.namespace)
                        if ret:
-                           print("Job %s %s is deleted" % (
+                           logger.info("Job %s %s is deleted" % (
                                object.metadata.name, object.metadata.namespace))
                        else:
-                           print("Job %s %s delete failed" % (object.metadata.name, object.metadata.namespace))
+                           logger.info("Job %s %s delete failed" % (object.metadata.name, object.metadata.namespace))
 
            #拉取所有的pod，如果pod为完成状态，并且pod无owner_references，则删除
            pods = v1Core.list_pod_for_all_namespaces(watch=False)
@@ -75,16 +92,16 @@ def main():
                     if object.metadata.owner_references == None :
                         ret = delete_pod_namespace(v1Core, object.metadata.name, object.metadata.namespace)
                         if ret:
-                            print("Pod %s %s is deleted" % (object.metadata.name, object.metadata.namespace))
+                            logger.info("Pod %s %s is deleted" % (object.metadata.name, object.metadata.namespace))
                         else:
-                            print("Pod %s %s delete failed" % (object.metadata.name, object.metadata.namespace))
+                            logger.info("Pod %s %s delete failed" % (object.metadata.name, object.metadata.namespace))
 
        except Exception as err:
-           print("event watch process error,%s" % (err))
-           logging.exception(err)
+           logger.info("event watch process error,%s" % (err))
+           logger.exception(err)
            pass
 
-    print("Ended.")
+    logger.info("Ended.")
 
 if __name__ == '__main__':
     main()
